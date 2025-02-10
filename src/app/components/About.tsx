1c9f4a89f8b1ecf1829d541e6a08d36be226112f
John Swärd <johnsvard@outlook.com>
2025-02-10 12:35:30 +0100

    Styling changes

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -7,26 +7,33 @@ import { IconRow } from "./IconRow";
 export const About = () => {
   return (
     <div className="flex flex-col gap-6 justify-center items-center">
-      <div className="gap-4 sm:gap-2  flex flex-col justify-center items-center">
-        <div className="flex flex-row align-center justify-center gap-1">
+      <div className="gap-2 sm:gap-2  flex flex-col">
+        <div className="flex flex-row gap-1">
           <LocationOnIcon className="w-6 h-6 text-gray-500 dark:text-gray-400" />
           <p className="text-gray-500 dark:text-gray-400 font-semibold">
             {"Sweden"}
           </p>
         </div>
-        <h1 className="flex flex-wrap gap-2 sm:gap-4 justify-center font-semibold dark:text-slate-200 text-gray-800 text-5xl">
-          {"Hi, I'm"}<span className="text-blue-400">John!</span>
-        </h1>
-        <h2 className="font-regular text-pretty text-gray-800 dark:text-gray-200 text-xl">
-          {"Software Developer & Student"}
-        </h2>
-        <p className="font-light text-pretty text-muted-foreground leading-none">
-          {
-            "Currently studying a BSc in Information Systems. In the meantime, I enjoy building cool things."
-          }
-        </p>
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-row gap-3 sm:gap-4 text-5xl">
+            <h1 className="flex flex-wrap font-semibold dark:text-slate-200 text-gray-800 ">
+              {"Hi, I'm"}
+            </h1>
+            <span className="text-blue-400">John!</span>
+          </div>
+          <h2 className="font-regular text-pretty text-gray-800 dark:text-gray-200 text-xl">
+            {"Software Developer & Student"}
+          </h2>
+          <p className="font-light text-pretty text-muted-foreground leading-none">
+            {
+              "Currently studying a BSc in Information Systems. In the meantime, I enjoy building cool things."
+            }
+          </p>
+        </div>
+        <span className="mt-2">
+          <IconRow isButtonEnabled={true} />
+        </span>
       </div>
-      <IconRow isButtonEnabled={true} />
     </div>
   );
 };
